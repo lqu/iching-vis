f7e597eb45ff734ad223566b02c9b44c3f18da17
Liang Qu <lqu@users.noreply.github.com>
2023-12-12 23:02:19 -0800

    Initial commit. Definition and JSON + D3 visualization generation.

---
+++ gen_json.py
@@ -0,0 +1,27 @@
+import hexagrams as h
+import json
+
+d = dict()
+d["nodes"] = []
+d["links"] = []
+
+for hexagram in h.hexagrams_binary:
+  # hexagram is a str like '111111' or '000000'
+  num = int(hexagram, 2)
+  d["nodes"] += [{"id": num, "name": hexagram}]
+
+#print(d)
+
+for hexagram in h.hexagrams_binary:
+  num = int(hexagram, 2)
+  for j in [1, 2, 4, 8, 16, 32]:
+    x = num ^ j
+    d["links"] += [{"source": num, "target": x}]
+
+#print(d)
+print(len(d["nodes"]))
+print(len(d["links"]))
+
+datastr = json.dumps(d, indent=2)
+with open('d3-graph.json', 'w') as outfile:
+  outfile.write(datastr)
